d1a51d8aa1a31f523e8c7ab9ba41ad5c9b18e560
wooyeeyii <wooyeeyii@163.com>
2019-04-04 16:58:16 +0800

    0404

--- leetcode/src/main/java/com/chang/leetcode/Problem91.java
+++ leetcode/src/main/java/com/chang/leetcode/Problem91.java
@@ -23,47 +23,36 @@ package com.chang.leetcode;
 
 public class Problem91 {
 
+    // s(n) = s(n-1) + (s(n-2),后2数)
     public int numDecodings(String s) {
-        if (0 == s.length()) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
-        if (1 == s.length()) {
-            if (s.charAt(0) == '0') {
-                return 0;
-            } else {
-                return 1;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
             }
         }
-
-        char last = s.charAt(0);
-        int pre = 0;
-        int res = last == '0' ? 0 : 1;
-        for (int i = 1; i < s.length(); i++) {
-            last = s.charAt(i - 1);
-            char add = s.charAt(i);
-            if (last == '0' && add == '0') {
-               return 0;
-            } else if('1' == last || ('2' == last && add <= '6' && add > '0')) {
-                res = pre * 2 + res;
-            } else {
-                pre = 1;
-            }
-        }
-        return 0;
-    }
-
-    public int numDecdingsRec(String s, Character newChar) {
-        if (null == s) {
-            return 1;
-        }
-
-        if (newChar <= '6' && s.charAt(s.length() - 1) <= '2') {
-
-        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
         Problem91 problem = new Problem91();
+        System.out.println(0 == problem.numDecodings("00"));
+        System.out.println(0 == problem.numDecodings("01"));
+        System.out.println(0 == problem.numDecodings("012"));
+        System.out.println(1 == problem.numDecodings("10"));
+        System.out.println(0 == problem.numDecodings("1002"));
+        System.out.println(1 == problem.numDecodings("101"));
         System.out.println(2 == problem.numDecodings("12"));
         System.out.println(3 == problem.numDecodings("226"));
     }
---
+++ leetcode/src/main/java/com/chang/leetcode/Problem93.java
@@ -0,0 +1,61 @@
+/**
+ * 93. Restore IP Addresses
+ * <p>
+ * Given a string containing only digits, restore it
+ * by returning all possible valid IP address combinations.
+ * <p>
+ * Example:
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ */
+package com.chang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem93 {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<String>();
+        int n = s.length();
+        restoreDiv(s, 0, 4, new ArrayList<String>(), res);
+        return res;
+    }
+
+    private void restoreDiv(String s, int index, int left, ArrayList<String> list, List<String> res) {
+        if(s.length() == index && left == 0) {
+            StringBuilder sb = new StringBuilder();
+            list.forEach(str -> sb.append(str).append("."));
+            String oneRes = sb.toString();
+            res.add(oneRes.substring(0, oneRes.length() - 1));
+        } else if (s.length() == index || left == 0) {
+            return;
+        }
+        if( ((s.length() - index) < left) || ((s.length() - index) > 3 * left)) {
+            return;
+        }
+
+        for(int i = 1; i <= 3 && index + i <= s.length(); i++) {
+            int next = Integer.valueOf(s.substring(index, index + i));
+            if(next > 255 || (i > 1 && "0".equals(s.substring(index, index + 1)))) {
+                return;
+            }
+            list.add(s.substring(index, index + i));
+            restoreDiv(s, index + i, left - 1, list, res);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem93 problem = new Problem93();
+
+//        List<String> list1 = problem.restoreIpAddresses("25525511135");
+//        // output: ["255.255.11.135", "255.255.111.35"]
+//        list1.forEach(s -> System.out.println(s));
+//        System.out.println("#################");
+
+        List<String> list2 = problem.restoreIpAddresses("010010");
+        // output: ["0.10.0.10","0.100.1.0"]
+        list2.forEach(s -> System.out.println(s));
+        System.out.println("#################");
+    }
+}
