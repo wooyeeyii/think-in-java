8c10f61fb0f3221ed26075a9e6b709726920b39c
wooyeeyii <wooyeeyii@163.com>
2019-04-16 23:44:19 +0800

    190416

---
+++ leetcode/src/main/java/com/chang/leetcode/tree/traversal/Problem144.java
@@ -0,0 +1,79 @@
+/**
+ * 144 Binary Tree Preorder Traversal
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * Example:
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,2,3]
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+package com.chang.leetcode.tree.traversal;
+
+import com.chang.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Problem144 {
+    // 递归
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(root, result);
+        return result;
+    }
+    private void preorderTraversal(TreeNode node, List<Integer> list) {
+        if(null == node) {
+            return;
+        }
+        list.add(node.val);
+        preorderTraversal(node.left, list);
+        preorderTraversal(node.right, list);
+    }
+
+    //迭代
+    public List<Integer> preorderTraversalIter(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(null != root) {
+            stack.push(root);
+        }
+        while(stack.size() > 0) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if(null != node.right) {
+                stack.push(node.right);
+            }
+            if(null != node.left) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+
+    // Note that in this solution only right children are stored to stack.
+    public List<Integer> preorderTraversalIterImpro(TreeNode node) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> rights = new Stack<TreeNode>();
+        while(node != null) {
+            result.add(node.val);
+            if (node.right != null) {
+                rights.push(node.right);
+            }
+            node = node.left;
+            if (node == null && !rights.isEmpty()) {
+                node = rights.pop();
+            }
+        }
+        return result;
+    }
+
+}
---
+++ leetcode/src/main/java/com/chang/leetcode/tree/traversal/Problem145.java
@@ -0,0 +1,42 @@
+/**
+ * 145. Binary Tree Postorder Traversal
+ *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [3,2,1]
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+package com.chang.leetcode.tree.traversal;
+
+import com.chang.common.TreeNode;
+
+import java.util.*;
+
+public class Problem145 {
+
+    // 递归
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderTraversal(root, result);
+        return result;
+    }
+    private void postorderTraversal(TreeNode node, List<Integer> list) {
+        if(null == node) {
+            return;
+        }
+        postorderTraversal(node.left, list);
+        postorderTraversal(node.right, list);
+        list.add(node.val);
+    }
+
+}
