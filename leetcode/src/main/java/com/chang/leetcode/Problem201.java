876afd8a26dcf0d5b8841270a5989f5703ccb96a
wooyeeyii <wooyeeyii@163.com>
2019-04-28 20:36:59 +0800

    0428

--- leetcode/src/main/java/com/chang/leetcode/Problem201.java
+++ leetcode/src/main/java/com/chang/leetcode/Problem201.java
@@ -17,26 +17,39 @@ package com.chang.leetcode;
 public class Problem201 {
 
     public int rangeBitwiseAnd(int m, int n) {
-        int mC = bitCount(m);
-        int nC = bitCount(n);
-        if(mC != nC) {
+        String ms = intToBinaryStr(m);
+        String ns = intToBinaryStr(n);
+        if(ms.length() != ns.length()) {
             return 0;
         }
+        int res = 0;
+        int i = ms.length() - 1;
+        while (i >= 0 && (('1' == ms.charAt(i) && '1' == ns.charAt(i)) ||
+                ('0' == ms.charAt(i) && '0' == ns.charAt(i)))) {
+            if('1' == ms.charAt(i)) {
+                res = res + (1 << i);
+            }
+            i--;
+        }
+        return res;
     }
 
-    private int bitCount(int n) {
-        int count = 0;
+    private String intToBinaryStr(int n) {
+        StringBuilder sb = new StringBuilder();
         while(n > 0) {
-            count++;
+            sb.append( n % 2);
             n = n >> 1;
         }
-        return count;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         Problem201 problem = new Problem201();
         System.out.println(4 == problem.rangeBitwiseAnd(5, 7));
         System.out.println(0 == problem.rangeBitwiseAnd(0, 1));
+        System.out.println(1 == problem.rangeBitwiseAnd(1, 1));
+        System.out.println(2 == problem.rangeBitwiseAnd(2, 2));
+        System.out.println(6 == problem.rangeBitwiseAnd(6, 6));
     }
 
 
