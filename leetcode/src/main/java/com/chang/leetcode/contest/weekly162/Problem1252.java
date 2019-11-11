5befb15e6713a2fc348f7f163a366162c09bf430
jj <wooyeeyii@163.com>
2019-11-11 10:42:20 +0800

    fix weekly contest

---
+++ leetcode/src/main/java/com/chang/leetcode/contest/weekly162/Problem1252.java
@@ -0,0 +1,55 @@
+/**
+ * 1252. Cells with Odd Values in a Matrix
+ *
+ * Given n and m which are the dimensions of a matrix initialized by zeros and given an array indices where indices[i] = [ri, ci].
+ * For each pair of [ri, ci] you have to increment all cells in row ri and column ci by 1.
+ *
+ * Return the number of cells with odd values in the matrix after applying the increment to all indices.
+ *
+ * Example 1:
+ *
+ * Input: n = 2, m = 3, indices = [[0,1],[1,1]]
+ * Output: 6
+ * Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+ * After applying first increment it becomes [[1,2,1],[0,1,0]].
+ * The final matrix will be [[1,3,1],[1,3,1]] which contains 6 odd numbers.
+ *
+ * Example 2:
+ *
+ * Input: n = 2, m = 2, indices = [[1,1],[0,0]]
+ * Output: 0
+ * Explanation: Final matrix = [[2,2],[2,2]]. There is no odd number in the final matrix.
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 50
+ * 1 <= m <= 50
+ * 1 <= indices.length <= 100
+ * 0 <= indices[i][0] < n
+ * 0 <= indices[i][1] < m
+ */
+package com.chang.leetcode.contest.weekly162;
+
+public class Problem1252 {
+
+    public int oddCells(int n, int m, int[][] indices) {
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+
+        for (int[] ind : indices) {
+            rows[ind[0]] += 1;
+            cols[ind[1]] += 1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((rows[j] + cols[i]) % 2 == 1) {
+                    count++;
+                }
+            }
+        }
+        return count++;
+    }
+
+}
--- leetcode/src/main/java/com/chang/leetcode/contest/weekly162/Problem1254.java
+++ leetcode/src/main/java/com/chang/leetcode/contest/weekly162/Problem1254.java
@@ -1,6 +1,38 @@
+/**
+ * 1254. Number of Closed Islands
+ *
+ * Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally
+ * (all left, top, right, bottom) surrounded by 1s.
+ *
+ * Return the number of closed islands.
+ *
+ * Example 1:
+ * Input: grid = [[1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
+ * Output: 2
+ * Explanation:
+ * Islands in gray are closed because they are completely surrounded by water (group of 1s).
+ *
+ * Example 2:
+ * Input: grid = [[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0]]
+ * Output: 1
+ *
+ * Example 3:
+ * Input: grid = [[1,1,1,1,1,1,1],
+ *                [1,0,0,0,0,0,1],
+ *                [1,0,1,1,1,0,1],
+ *                [1,0,1,0,1,0,1],
+ *                [1,0,1,1,1,0,1],
+ *                [1,0,0,0,0,0,1],
+ *                [1,1,1,1,1,1,1]]
+ * Output: 2
+ *
+ * Constraints:
+ * 1 <= grid.length, grid[0].length <= 100
+ * 0 <= grid[i][j] <=1
+ */
 package com.chang.leetcode.contest.weekly162;
 
-public class Problem5257 {
+public class Problem1254 {
 
     private int rows = 0;
     private int cols = 0;
@@ -54,8 +86,8 @@ public class Problem5257 {
     }
 
     public static void main(String[] args) {
-        Problem5257 problem = new Problem5257();
-        /*int[][] grid1 = new int[][]{
+        Problem1254 problem = new Problem1254();
+        int[][] grid1 = new int[][]{
                 {1, 1, 1, 1, 1, 1, 1, 0},
                 {1, 0, 0, 0, 0, 1, 1, 0},
                 {1, 0, 1, 0, 1, 1, 1, 0},
@@ -80,10 +112,10 @@ public class Problem5257 {
                 {0, 1, 0, 1, 0},
                 {0, 1, 1, 1, 0}
         };
-        System.out.println(1 == problem.closedIsland(grid3));*/
+        System.out.println(1 == problem.closedIsland(grid3));
 
         int[][] grid4 = new int[][]{{1}};
-        System.out.println(1 == problem.closedIsland(grid4));
+        System.out.println(0 == problem.closedIsland(grid4));
     }
 
 }
